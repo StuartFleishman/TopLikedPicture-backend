4c390b69ec19c85b99b109579b16e28eb2c9b97e
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2021-06-20 18:57:32 -0400

    add user controller with create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
-  has_secure_password
+
+  def create 
+    user = User.new(user_params)
+
+    if user.save 
+      render json: { status: 201, user: user, logged_in: true}
+    else 
+      render json: { status: 500, message: "There was an error in creating an account"}
+    end
+  end
+
+
+  private 
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+ 
 end
